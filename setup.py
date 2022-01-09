a901a820fb20daa491e22429f0aabc6b7d55e4b8
Victor Miguel Armenta Carrillo <victorarmenta30@gmail.com>
2022-01-09 14:24:35 -0600

    setup.py full working...

--- setup.py
+++ setup.py
@@ -1,18 +1,44 @@
 from setuptools import setup, find_packages
+from setuptools.command.sdist import sdist as base_sdist
+from setuptools.command.bdist_egg import bdist_egg as base_bdist_egg
+
 from distutils.command.build import build
+from distutils import log
+
+import subprocess
 
 from wagtailiconify import __version__
 
-class NPMInstall(build):
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+class assets_mixin:
+    def compile_assets(self):
+        try:
+            subprocess.check_call(['npm', 'install'])
+            subprocess.check_call(['npm', 'run', 'build'])
+        except (OSError, subprocess.CalledProcessError) as e:
+            print('Error compiling assets: ' + str(e))
+            raise SystemExit(1)
+
+
+class sdist(base_sdist, assets_mixin):
     def run(self):
-        self.run_command("npm install")
-        self.run_command("npm build")
-        build.run(self)
+        self.compile_assets()
+        base_sdist.run(self)
+
+
+class bdist_egg(base_bdist_egg, assets_mixin):
+    def run(self):
+        self.compile_assets()
+        base_bdist_egg.run(self)
 
 setup(
     name='wagtailiconify',
     version=__version__,
     description='A plugin for Wagtail CMS, icon blocks (fontawesome)',
+    long_description=long_description,
     author='Nefonfo',
     author_email='victorarmenta30@gmail.com',
     url='https://github.com/Nefonfo/wagtailiconify',
@@ -35,6 +61,7 @@ setup(
         "Django>=3.2.0",
     ],
     cmdclass={
-        'npm_install': NPMInstall
+        'sdist': sdist,
+        'bdist_egg': bdist_egg,
     },
 )
